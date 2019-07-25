ba3fd1832683300a20e78d57e516cec7585c576f
Kirill Shmilovich <kshmilov@gmail.com>
2019-07-25 11:15:33 -0500

    made measure modeule

--- geometry_analysis/__init__.py
+++ geometry_analysis/__init__.py
@@ -5,6 +5,7 @@ A python package for the MolSSI Software Summer School.
 
 # Add imports here
 from .molecule import *
+from .measure import *
 
 # Handle versioneer
 from ._version import get_versions
--- geometry_analysis/molecule.py
+++ geometry_analysis/molecule.py
@@ -2,64 +2,11 @@
 molecule.py
 A python package for the MolSSI Software Summer School.
 
-Contains a molecule class
+Contains a Molecule class
 """
 
 import numpy as np
-
-
-def calculate_distance(rA, rB):
-    """Calculate the distance between points A and B.
-    
-    Parameters
-    ----------
-    rA : numpy array
-        The x, y, z coordinates of point A
-    rB : numpy array
-        The x, y, z coordinates of point B
-    
-    Returns
-    -------
-    distance : float
-        The distance between points A and B.
-    
-    Examples
-    --------
-    >>> calculate_distance(np.array([0, 0, 0], [0, 0.1, 0]))
-    0.1
-    """
-    dist_vec = (rA - rB)
-    distance = np.linalg.norm(dist_vec)
-    return distance
-
-
-def calculate_angle(rA, rB, rC, degrees=False):
-    """Calculate angle between points A, B, and C
-    
-    Parameters
-    ----------
-    rA : numpy array
-        The x, y, z coordinates of point A
-    rB : numpy array
-        The x, y, z coordinates of point B
-    degrees : bool, optional
-        Return the calculated angle in degrees.
-    
-    Returns
-    -------
-    angle : float
-        The distance between points A and B.
-    """
-    AB = rB - rA
-    BC = rB - rC
-
-    theta = np.arccos(np.dot(AB, BC) / (np.linalg.norm(AB) * np.linalg.norm(BC)))
-
-    if degrees:
-        return np.degrees(theta)
-    else:
-        return theta
-
+from .measure import calculate_angle, calculate_distance
 
 class Molecule:
     def __init__(self, name, symbols, coordinates):
