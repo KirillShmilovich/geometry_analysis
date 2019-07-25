a428ff0da154bbd4544ff451bbb81addbc3a799a
Kirill Shmilovich <kshmilov@gmail.com>
2019-07-25 10:23:39 -0500

    yapf molecule.py

--- geometry_analysis/molecule.py
+++ geometry_analysis/molecule.py
@@ -1,34 +1,133 @@
 """
 molecule.py
-A python package for the MolSSI Software Summer School. 
+A python package for the MolSSI Software Summer School.
 
-Handles the primary functions
+Contains a molecule class
 """
 
+import numpy as np
 
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format)
 
-    Replace this function and doc string for your own project
-
+def calculate_distance(rA, rB):
+    """Calculate the distance between points A and B.
+    
     Parameters
     ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from
-
+    rA : numpy array
+        The x, y, z coordinates of point A
+    rB : numpy array
+        The x, y, z coordinates of point B
+    
     Returns
     -------
-    quote : str
-        Compiled string including quote and optional attribution
+    distance : float
+        The distance between points A and B.
+    
+    Examples
+    --------
+    >>> calculate_distance(np.array([0, 0, 0], [0, 0.1, 0]))
+    0.1
     """
+    dist_vec = (rA - rB)
+    distance = np.linalg.norm(dist_vec)
+    return distance
 
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
+
+def calculate_angle(rA, rB, rC, degrees=False):
+    """Calculate angle between points A, B, and C
+    
+    Parameters
+    ----------
+    rA : numpy array
+        The x, y, z coordinates of point A
+    rB : numpy array
+        The x, y, z coordinates of point B
+    degrees : bool, optional
+        Return the calculated angle in degrees.
+    
+    Returns
+    -------
+    angle : float
+        The distance between points A and B.
+    """
+    AB = rB - rA
+    BC = rB - rC
+
+    theta = np.arccos(np.dot(AB, BC) / (np.linalg.norm(AB) * np.linalg.norm(BC)))
+
+    if degrees:
+        return np.degrees(theta)
+    else:
+        return theta
+
+
+class Molecule:
+    def __init__(self, name, symbols, coordinates):
+        if isinstance(name, str):
+            self.name = name
+        else:
+            raise TypeError("Name is not a string.")
+
+        self.symbols = symbols
+        self._coordinates = coordinates
+        self.bonds = self.build_bond_list()
+
+    @property
+    def num_atoms(self):
+        return len(self.coordinates)
+
+    @property
+    def coordinates(self):
+        return self._coordinates
+
+    @coordinates.setter
+    def coordinates(self, new_coordinates):
+        self._coordinates = new_coordinates
+        self.bonds = self.build_bond_list()
+
+    def build_bond_list(self, max_bond=2.93, min_bond=0):
+        """
+        Build a list of bonds based on a distance criteria.
+
+        Atoms within a specified distance of one another will be considered bonded.
+
+        Parameters
+        ----------
+        max_bond : float, optional
+
+        min_bond : float, optional
+
+        Returns
+        -------
+        bond_list : list
+            List of bonded atoms. Returned as list of tuples where the values are the atom indices.
+        """
+
+        bonds = {}
+
+        for atom1 in range(self.num_atoms):
+            for atom2 in range(atom1, self.num_atoms):
+                distance = calculate_distance(self.coordinates[atom1], self.coordinates[atom2])
+
+                if distance > min_bond and distance < max_bond:
+                    bonds[(atom1, atom2)] = distance
+
+        return bonds
 
 
 if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
+    random_coordinates = np.random.random([3, 3])
+    name = "my molecule"
+    symbols = ["H", "O", "H"]
+
+    my_molecule = Molecule(name, symbols, random_coordinates)
+
+    print(F'There are {len(my_molecule.bonds)} bonds')
+    print(F'The coordinates are {my_molecule.coordinates}')
+
+    random_coordinates[0] += 100
+
+    my_molecule.coordinates = random_coordinates
+
+    print(F'\n\nThere are {len(my_molecule.bonds)} bonds')
+    print(F'The coordinates are {my_molecule.coordinates}')
